6455c42ced8a69ed2cf884ffc3da2b30b71302c3
alyson <consulialyson@gmail.com>
2022-09-07 03:15:20 -0300

    feat: tests to register activities

--- tests/integration/activities.test.ts
+++ tests/integration/activities.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import app, { init } from '@/app';
+import app, { close, init } from '@/app';
 import httpStatus from 'http-status';
 import supertest from 'supertest';
 import { cleanDb, generateValidToken } from '../helpers';
@@ -10,6 +10,8 @@ import * as formats from '../../src/utils/formats-utils';
 import { createUser } from '../factories';
 import { createActivity } from '../factories/activities-factory';
 import { createLocation } from '../factories/locations-factory';
+import { prisma } from '@/config';
+import { createTicket } from '../factories/tickets-factory';
 
 beforeAll(async () => {
   await init();
@@ -19,6 +21,11 @@ beforeAll(async () => {
 const server = supertest(app);
 
 describe('GET /activities', () => {
+  beforeAll(async () => {
+    await init();
+    await cleanDb();
+  });
+
   it('should respond with status 401 if no token is given', async () => {
     const response = await server.get('/activities');
 
@@ -68,4 +75,78 @@ describe('GET /activities', () => {
       });
     });
   });
+});
+
+describe('POST /activities/register', () => {
+  beforeAll(async () => {
+    await init();
+    await cleanDb();
+  });
+
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.post('/activities/register');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.post('/activities/register').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/activities/register').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('when token is valid', () => {
+    it('should respond with status 400 when body is not present', async () => {
+      const token = await generateValidToken();
+
+      const response = await server.post('/activities/register').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.BAD_REQUEST);
+    });
+
+    it('should respond with status 400 when body is not valid', async () => {
+      const token = await generateValidToken();
+      const body = { [faker.lorem.word()]: faker.lorem.word() };
+
+      const response = await server.post('/activities/register').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toBe(httpStatus.BAD_REQUEST);
+    });
+  });
+
+  describe('when body is valid', () => {
+    const generateValidBody = (id: number) => ({
+      id,
+    });
+
+    it('should respond with status 200 and connect user to the activity', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      await createTicket(user);
+      const activityData = await createActivity();
+      const { id } = activityData;
+      const body = generateValidBody(id);
+
+      let response = await server.post('/activities/register').set('Authorization', `Bearer ${token}`).send(body);
+      expect(response.status).toBe(httpStatus.OK);
+
+      response = await server.get('/activities').set('Authorization', `Bearer ${token}`);
+      expect(response.body.activities[0].isRegister).toBe(true);
+    });
+  });
+});
+
+afterAll(async () => {
+  await close();
 });
